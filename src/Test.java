9a2590a46dc5f992eabf50cb566268669963db16
sdj <1580987935@qq.com>
2023-05-26 15:45:30 +0800

    新增坦克重叠情况分析图

--- src/Test.java
+++ src/Test.java
@@ -2,34 +2,40 @@ import java.io.*;
 import java.util.Properties;
 
 class Test {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         Properties properties = new Properties();
-        //将FileInputStream流指定的配置文件的键值对，加载到Properties对象中
-//        properties.load(new FileInputStream("./testFolder/test.properties"));
+        properties.load(new FileReader("./testFolder/dog.properties"));
+        String name = properties.getProperty("name");
+        int age = new Integer(properties.getProperty("age"));
+//        int age = Integer.getInteger(properties.getProperty("age"));
+        String color = properties.getProperty("color");
+        Dog dog = new Dog(name, age, color);
+        System.out.println(dog);
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("./testFolder/dog.dat"));
+        objectOutputStream.writeObject(dog);
+        objectOutputStream.close();
+        ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("./testFolder/dog.dat"));
+        System.out.println(objectInputStream.readObject());
+    }
+}
 
-        //将FileReader流指定的配置文件的键值对，加载到Properties对象中
-        properties.load(new FileReader("./testFolder/test.properties"));
+class Dog implements Serializable {
+    private String name;
+    private int age;
+    private String color;
 
-        //将Properties对象中保存的键值对通过标准输出流的println方法输出到显示器。
-        properties.list(System.out);
+    public Dog(String name, int age, String color) {
+        this.name = name;
+        this.age = age;
+        this.color = color;
+    }
 
-        //将Properties对象中保存的键值对通过PrintWriter流的println方法输出到显示器。(因为PrintWriter包装了标准输出流，所以底层调用的还是标准
-        // 输出流的println方法)。
-//        PrintWriter printWriter = new PrintWriter(System.out);
-//        properties.list(printWriter);
-//        如果之后还有内容输出到显示器上，就不要关闭标准输出流
-//        printWriter.close();
-
-        //得到key=user的value,不存在则返回null
-        System.out.println(properties.getProperty("user"));
-        //得到key=haha的value,不存在则返回指定的默认值
-//        System.out.println(properties.getProperty("haha", "no exist"));
-        //修改key=user1的value，key不存在则新增该key-value对
-        System.out.println(properties.setProperty("user", "sdj5哈"));
-
-        //将Properties中的键值对存储到OutPutStream流指定的配置文件中。
-//        properties.store(new FileOutputStream("./testFolder/test.properties"), "进行存储");
-        //将Properties中的键值对存储到Writer流指定的配置文件中，并不写注释。
-        properties.store(new FileWriter("./testFolder/test.properties"), null);
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", color='" + color + '\'' +
+                '}';
     }
 }
